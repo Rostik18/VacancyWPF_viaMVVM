4f49a4ff600fdd07a955570b8b8019c5a599a44b
Rostik <rostik1804@gmail.com>
2019-04-10 23:27:47 +0300

    Add Model

---
+++ Vacancy/Worker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vacancy
+{
+    public class Worker
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public DateTime Birthday { get; set; }
+
+    }
+}
